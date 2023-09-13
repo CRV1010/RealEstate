199f8e846b08a52a0a7c539acb3b7686f09fc58d
Sahil18072003 <sahildharaviya18@gmail.com>
2023-09-13 23:38:21 +0530

    ContactUs

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'cyan': colors.cyan,
+        'teal': colors.teal
+      }
+    },
   },
   plugins: [],
 }
